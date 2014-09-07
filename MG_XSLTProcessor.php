5b277028299c8d56dbfee1d40c5535eb68eb8795
Marco Germani <necrox87@gmail.com>
2014-09-07 11:38:58 +0200

    Update MG_XSLTProcessor.php

--- MG_XSLTProcessor.php
+++ MG_XSLTProcessor.php
@@ -2,20 +2,19 @@
 
 require_once("interface/MG_XSLTProcessor_Interface.php");
 
-class MG_XSLTProcessor extends XSLTProcessor implements MG_XSLTProcessor_Interface {
-	
+class MG_XSLTProcessor extends XSLTProcessor implements MG_XSLTProcessor_Interface 
+{
 	private $stylesheet = null;
 	private $params = array();
 	
-	public function importStyleSheet($stylesheet) {
-
+	public function importStyleSheet($stylesheet) 
+	{
 		$this->stylesheet = $stylesheet;
 	}
 
-	public function setParameterById($id, $attribute, $value) {
-		
+	public function setParameterById($id, $attribute, $value) 
+	{
 		if( !isset($this->params[trim($id)]) ) {
-			
 			$this->params[trim($id)] = array(
 				'attribute' => trim($attribute),
 				'value' => trim($value)
@@ -23,31 +22,30 @@ class MG_XSLTProcessor extends XSLTProcessor implements MG_XSLTProcessor_Interfa
 		}			
 	}
 	
-	public function transformToDoc($doc) {
-		
+	public function transformToDoc($doc) 
+	{
 		$this->preProcess();
 		return parent::transformToDoc($doc);
 	}
-	public function transformToURI($doc, $uri) {
-		
+	public function transformToURI($doc, $uri) 
+	{
 		$this->preProcess();
 		return parent::transformToURI($doc, $uri);
 	}
 	
-	public function transformToXML($doc) {
-		
+	public function transformToXML($doc) 
+	{
 		$this->preProcess();
 		return parent::transformToXML($doc);
 	}	
 	
-	private function preProcess() {
-		
+	private function preProcess() 
+	{
 		if( !empty($this->params) && $this->stylesheet instanceof DOMDocument) {
 			
 			$xp = new DomXPath($this->stylesheet);
 			
 			foreach($this->params as $id => $v) {
-			
 				$res = $xp->query("//*[@id = '$id']");
 				$res->item(0)->setAttribute($v['attribute'], $v['value']);	
 			}
