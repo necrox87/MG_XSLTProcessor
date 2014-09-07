1c882d8b028099318bf079f6077204edeace3f83
Marco Germani <necrox87@gmail.com>
2014-09-07 11:36:45 +0200

    Update MG_XSLTProcessor_Interface.php

--- interface/MG_XSLTProcessor_Interface.php
+++ interface/MG_XSLTProcessor_Interface.php
@@ -1,1 +1,12 @@
-<?phpinterface MG_XSLTProcessor_Interface {	/* Interface for native XSLTProcessor methods */	public function importStyleSheet($stylesheet);	public function transformToDoc($doc);	public function transformToURI($doc, $uri);	public function transformToXML($doc);}?>
+<?php
+
+interface MG_XSLTProcessor_Interface 
+{
+	/* Interface for native XSLTProcessor methods */
+	public function importStyleSheet($stylesheet);
+	public function transformToDoc($doc);
+	public function transformToURI($doc, $uri);
+	public function transformToXML($doc);
+}
+
+?>
